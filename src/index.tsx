52c531048f2a930b04d044c79df601644b0165f2
teay <teay20000@gmail.com>
2026-01-01 16:16:23 +0700

    fix: Temporarily disable dayjs locale setting to fix build
    This commit comments out the global  locale setting in . This was necessary to resolve a build failure that occurred when deploying to GitHub Pages.
    
    The  locale setting was previously introduced in an attempt to influence the  input's display in the edit modal. However, it caused a critical build issue.
    
    Further investigation is needed to achieve consistent 24-hour time display in the edit modal without breaking the build process.

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import dayjs from 'dayjs';
-import 'dayjs/locale/en-gb'; // Import the desired locale
+// import 'dayjs/locale/en-gb'; // Import the desired locale
 
 import App from './App';
 import './styles/Global';
 
 import { SettingsProvider } from './contexts/SettingsContext';
 
-dayjs.locale('en-gb'); // Set the global locale
+// dayjs.locale('en-gb'); // Set the global locale
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
