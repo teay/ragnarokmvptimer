8f1575dc2341028b19c60dccbace6a8d833b764b
teay <teay20000@gmail.com>
2026-06-10 18:22:52 +0700

    Fix: Revert modal alignment to center

--- src/modals/ModalBase/styles.ts
+++ src/modals/ModalBase/styles.ts
@@ -3,15 +3,13 @@ import { styled } from '@linaria/react';
 export const Container = styled.div`
   display: flex;
   justify-content: center;
-  align-items: flex-start; /* Changed from center to flex-start */
+  align-items: center; /* Reverted back to center */
 
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-
-  height: 100%;
 
   background-color: var(--modal_bg);
   backdrop-filter: var(--modal_backdrop_filter);
