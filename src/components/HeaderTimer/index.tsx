2480b660add1e84b2643721d3206e93c7c1ea6ba
teay <teay20000@gmail.com>
2026-06-10 21:09:55 +0700

    feat: Add Thai date format with adjusted spacing to HeaderTimer

--- src/components/HeaderTimer/index.tsx
+++ src/components/HeaderTimer/index.tsx
@@ -7,8 +7,16 @@ type Props = {
 
 export function HeaderTimer({ use24HourFormat }: Props) {
   const { now } = useTimer();
+  const thaiNow = now.locale('th'); // Force Thai locale for this component
+
+  const time = thaiNow.format(use24HourFormat ? 'HH:mm:ss' : 'hh:mm:ss A');
+  const day = thaiNow.format('ddd'); // e.g., พุธ
+  const date = `${thaiNow.format('D/M/')}${thaiNow.year() + 543}`; // e.g., 10/6/2569
 
   return (
-    <Hour>{now.format(use24HourFormat ? 'HH:mm:ss' : 'hh:mm:ss A')}</Hour>
+    <Hour>
+      {time} &nbsp; &nbsp;{day} {date}
+    </Hour>
   );
 }
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import dayjs from 'dayjs';
-// import 'dayjs/locale/en-gb'; // Import the desired locale
+import 'dayjs/locale/th';
 
 import App from './App';
 import './styles/Global';
@@ -9,7 +9,7 @@ import './styles/Global';
 import { SettingsProvider } from './contexts/SettingsContext';
 import { TimerProvider } from './contexts/TimerContext';
 
-// dayjs.locale('en-gb'); // Set the global locale
+dayjs.locale('th');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
