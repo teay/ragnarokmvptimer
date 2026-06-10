52874a8613d639fa9b7a68269a6142cf8d5a9ea1
teay <teay20000@gmail.com>
2026-06-10 19:28:22 +0700

    feat: remove black background for map to restore transparency

--- src/components/Map/styles.ts
+++ src/components/Map/styles.ts
@@ -9,7 +9,6 @@ export const MapImg = styled.img<MapImgProps>`
   width: 100%;
   height: 25rem;
   object-fit: contain;
-  background-color: #000;
   cursor: ${({ clickable }) => (clickable ? 'pointer' : 'default')};
   border: none;
 `;
