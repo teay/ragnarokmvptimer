f2533296f22c57a023b36cfd556939080cff1cbc
teay <teay20000@gmail.com>
2026-06-10 19:29:47 +0700

    feat: use semi-transparent background for map

--- src/components/Map/styles.ts
+++ src/components/Map/styles.ts
@@ -9,6 +9,7 @@ export const MapImg = styled.img<MapImgProps>`
   width: 100%;
   height: 25rem;
   object-fit: contain;
+  background-color: rgba(0, 0, 0, 0.3);
   cursor: ${({ clickable }) => (clickable ? 'pointer' : 'default')};
   border: none;
 `;
